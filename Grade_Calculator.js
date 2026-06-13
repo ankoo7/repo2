49b35be434c15c4e7246f91f1a7cea6b521a07bd
ankoo7 <aakankshak907@gmail.com>
2026-06-13 20:58:56 +0530

    commit1

---
+++ Grade_Calculator.js
@@ -0,0 +1,11 @@
+let percentage =30;
+if(percentage>=90){
+    console.log('A Grade - Congratulations! Distinction')
+}
+else if(percentage>=75){
+    console.log('B Grade - Well done')
+}
+else if(percentage>=60){
+    console.log('C Grade - You passed')
+}
+else{ console.log('Fail - Better luck next time')}
